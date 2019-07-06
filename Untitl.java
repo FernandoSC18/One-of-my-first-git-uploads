7c43d16be38a7c65d5dbb34a6b1cfc17a3f9f8fa
Fernando <f-ercho44@hotmail.com>
2019-07-06 02:57:26 -0500

    VERSION 1.0.0.1

---
+++ Untitl.java
@@ -0,0 +1,22 @@
+//progrma JAVA
+
+
+Class Principal {
+
+    public static void main (String args[]){
+
+        Principal nuevo = new Principal();
+        int Numero = nuevo.Iniciar();
+
+        System.out.println ("El nuevo numero seleccionado es el siguiente: " + Numero);
+
+    }
+
+
+    public int Iniciar (){
+
+        return 1;
+    }
+
+
+}
---
+++ Untitled-11.java
@@ -0,0 +1,22 @@
+//progrma JAVA
+
+
+Class Principal {
+
+    public static void main (String args[]){
+
+        Principal nuevo = new Principal();
+        int Numero = nuevo.Iniciar();
+
+        System.out.println ("El numero seleccionado es el siguiente XD: " + Numero);
+
+    }
+
+
+    public int Iniciar (){
+
+        return 1;
+    }
+
+
+}
